ac7f684ea56e938da1ecbea54a023974f45a1961
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-03-23 18:49:10 -0500

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -22,7 +22,7 @@ SPANISH=0
 
 #BASE_SOURCE_PATH="https://multi.netlify.app"
 #BASE_SOURCE_PATH="https://raw.githubusercontent.com/ThonyDroidYT/multi-v2ray/master"
-BASE_SOURCE_PATH="http://v2ray.admplus.tk"
+BASE_SOURCE_PATH="https://v2ray.admplus.tk"
 
 UTIL_PATH="/etc/v2ray_util/util.cfg"
 
