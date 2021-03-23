aee4ab401fa0404a0306f7bfa82e4524eaa0c3f4
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-03-23 18:18:00 -0500

    Update v2ray_backup.sh

--- v2ray_backup.sh
+++ v2ray_backup.sh
@@ -18,9 +18,9 @@ REMOVE=0
 
 CHINESE=0
 
-#BASE_SOURCE_PATH="https://multi.netlify.app"
+BASE_SOURCE_PATH="https://multi.netlify.app"
 #BASE_SOURCE_PATH="https://raw.githubusercontent.com/ThonyDroidYT/multi-v2ray/master"
-BASE_SOURCE_PATH="http://v2ray.admplus.tk"
+#BASE_SOURCE_PATH="http://v2ray.admplus.tk"
 
 UTIL_PATH="/etc/v2ray_util/util.cfg"
 
