acb66076d6d737b81e0ee4b5aa9998472d24ead3
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-04-14 17:23:04 -0500

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -250,7 +250,7 @@ installFinish() {
         #TLS
         wget -O /usr/local/lib/python3.5/dist-packages/v2ray_util/config_modify/tls.py https://raw.githubusercontent.com/ThonyDroidYT/multi-v2ray/master/v2ray_util/config_modify/tls.py &> /dev/null
         #MAIN
-        wget -O /usr/local/lib/python3.5/dist-packages/v2ray_util/main.py https://raw.githubusercontent.com/ThonyDroidYT/multi-v2ray/master/v2ray_util/main_es_backup.py.py &> /dev/null
+        wget -O /usr/local/lib/python3.5/dist-packages/v2ray_util/main.py https://raw.githubusercontent.com/ThonyDroidYT/multi-v2ray/master/v2ray_util/main.py &> /dev/null
         v2ray restart
         barra
         v2ray info
